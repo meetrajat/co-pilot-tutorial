833ac111ff656cb68cf0134ba508991d1db3185a
r0g0bq6 <meetrajatgupta@gmail.com>
2025-06-15 19:06:31 +0530

    Refactoring code to make it loosely coupled

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.apollographql.apollo3").version("3.8.3")
+}
+
+apollo {
+    service("MyGraphqlService") { // Name your service
+        packageName.set("com.example.a29th_mar_android_project") // Replace with your package
+        srcDir("src/main/graphql/countriesincontinent") // Where to save the generated code")  //Where to look for .graphql files. Replace the path if necessary
+    }
+}
+
+android {
+    namespace = "com.example.a29th_mar_android_project"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.a29th_mar_android_project"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.ktx)
+    implementation("com.apollographql.apollo3:apollo-runtime:3.8.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // Check for latest version
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Or other converter
+    implementation("com.squareup.okhttp3:okhttp:4.11.0") // Check for latest version
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0") // Check for latest version
+    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.mockito:mockito-android:5.5.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+}
